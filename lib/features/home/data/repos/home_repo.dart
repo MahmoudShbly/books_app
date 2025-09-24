2a59a55b2bd8ec321d0fdcb97301cb0e43cbe082
Mahmoud Alshbly <shbly1029@gmail.com>
2025-09-24 10:04:25 -0700

    create home repo and failures file

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,10 @@
+import 'package:books_app/core/errors/failures.dart';
+import 'package:books_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks ();
+  Future<Either<Failures,List<BookModel>>> fetchFeturedBooks();
+    
+
+}
